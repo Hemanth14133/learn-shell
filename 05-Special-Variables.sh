22574a63fd8c47a1348f335654d1b83935848654
Hemanth14133 <hemanth14133@gmail.com>
2023-09-26 17:23:46 +0530

    variables & functions

--- 05-Special-Variables.sh
+++ 05-Special-Variables.sh
@@ -1,7 +1,7 @@
 # SPECIAL VARIABLES ARE 0-n , * , @ , # , ?
-echo $0
-echo $1
-echo $2
-echo $@
-echo $*
-echo $#
+echo  Script name - $0
+echo  First arguement - $1
+echo  Second Argument - $2
+echo  All Arguments - $@
+echo  All Arguments - $*
+echo  NO. of Arguments - $#
