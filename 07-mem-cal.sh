302cc25a580606e57195b87281a26901c0712225
Hemanth14133 <hemanth14133@gmail.com>
2023-11-01 11:20:27 +0530

    variables & functions

--- 07-mem-cal.sh
+++ 07-mem-cal.sh
@@ -1,5 +1,17 @@
 total=$(free | grep Mem | awk '{print $2}')
 free=$(free | grep Mem | awk '{print $4}')
 
-free_percentage=$(echo "$free / $total * 100" | bc -l | awk -f . '{print $1}')
+free_percentage=$(echo "$free / $total * 100" | bc -l )
 echo $free_percentage
+
+# 0-50 used -> good
+# $1 - 70 - orange
+# >70 - red
+
+if [ $used_percentage -lt 50 ]; then
+  echo Mem stat is GREEN
+elif [ $used_percentage -lt 70 ]; then
+  echo Mem stat is ORANGE
+else
+  echo Mem stat is RED
+fi
